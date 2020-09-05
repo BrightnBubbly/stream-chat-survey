a3f608978ab5c99bb7651b58d8ed8fb6b62ba40d
User <keith.crosslin@gmail.com>
2020-09-05 15:12:34 +0200

    deleted mis-referenced file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,14 @@ import landimage from "./stream_and_surveylegend.png";
 function App() {
 
   return (
-      <div className="App">
-        <div className="App-header">
-          <p>Stream and SurveyLegend Integration</p>
-          <div><img src={landimage} alt="shows integration between Stream and SurveyLegend"></img></div>
-          <p>(This is an example landing page)</p>
-          <ChatWidget></ChatWidget>
-        </div>
+    <div className="App">
+      <div className="App-header">
+        <p>Stream and SurveyLegend Integration</p>
+        <div><img src={landimage} alt="shows integration between Stream and SurveyLegend"></img></div>
+        <p>(This is an example landing page)</p>
+        <ChatWidget></ChatWidget>
       </div>
+    </div>
   );
 }
 
